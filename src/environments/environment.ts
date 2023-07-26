a9dd844a4c5becad693541fbedf0a4d63215b7f7
vishal <poke.fcr@gmail.com>
2023-07-26 23:37:00 +0530

    upload gallery functinality partial implemented

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCIKLb3NTFhsJ6H1VpDdA69KzkxfRLsS4U",
+    authDomain: "impulse-classes-a913f.firebaseapp.com",
+    databaseURL: "https://impulse-classes-a913f-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "impulse-classes-a913f",
+    storageBucket: "impulse-classes-a913f.appspot.com",
+    messagingSenderId: "193231575860",
+    appId: "1:193231575860:web:d6f6b34380477c35ce6361",
+    measurementId: "G-PCYT7EXKJX"
+  },
+};
