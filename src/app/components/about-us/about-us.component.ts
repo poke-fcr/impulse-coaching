6a196c8481daa582dd02af1a4af3d9ef54fcf87d
vishal <poke.fcr@gmail.com>
2023-08-20 18:21:51 +0530

    changes added

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -7,18 +7,75 @@ import anime from 'animejs/lib/anime.es.js';
   styleUrls: ['./about-us.component.scss'],
 })
 export class AboutUsComponent implements OnInit, AfterViewInit {
+  screenSizeLtMedium: boolean = window.innerWidth < 762 ? true : false;
+  tl = anime.timeline({
+    easing: 'easeOutExpo',
+    duration: 750,
+  });
+
   ngAfterViewInit() {
-    anime({
-      targets: '.line-drawing-demo .lines path',
-      strokeDashoffset: [anime.setDashoffset, 0],
-      easing: 'easeInOutSine',
-      duration: 3500,
-      delay: function (el, i) {
-        return i * 150;
-      },
+    let textWrapper = document.querySelector('.my-description');
+    if (textWrapper && textWrapper.textContent)
+      textWrapper.innerHTML = textWrapper.textContent.replace(
+        /\S/g,
+        "<span class='letter'>$&</span>"
+      );
+
+    if (!this.screenSizeLtMedium) {
+      this.tl.add({
+        targets: '.line-drawing-demo .lines path',
+        strokeDashoffset: [anime.setDashoffset, 0],
+        easing: 'easeInOutSine',
+        duration: 2000,
+        delay: function (el, i) {
+          return i * 150;
+        },
+      });
+     
+      
+    } else{
+      let textWrapper = document.querySelector('#heading');
+      if (textWrapper && textWrapper.textContent)
+        textWrapper.innerHTML = textWrapper.textContent.replace(
+          /\S/g,
+          "<span class='letter h1'>$&</span>"
+        );
+        this.tl.add({
+          targets: '#heading .letter',
+          opacity: [0, 1],
+          translateZ: 0,
+          easing: 'easeOutExpo',
+          duration: 500,
+          delay: (el, i) => 50 * i,
+        }, 1000);
+    }
+
+   
+   
+
+    this.tl.add({
+      targets: '#image-one',
+      translateX: ['-2500px', 0], // -> '250px'
+      opacity: [0, 1],
+      easing: 'easeOutExpo',
+      duration: 2000,
+    })
+
+    let textWrapper1 = document.querySelector('#text-one');
+      if (textWrapper1 && textWrapper1.textContent)
+        textWrapper1.innerHTML = textWrapper1.textContent.replace(
+          /\S/g,
+          "<span class='letter h4'>$&</span>"
+        );
+    this.tl.add({
+      targets: '#text-one .letter',
+      opacity: [0, 1],
+      translateZ: 0,
+      easing: 'easeOutExpo',
+      duration: 2000,
+      delay: (el, i) => 25 * i,
     });
+
   }
-  
-  title = 'hello world';
   ngOnInit(): void {}
 }
--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -9,16 +9,21 @@ export class GalleryComponent {
   imageIndex = 0;
   imageObjectArr = [
     {
-      image: '../../../assets/gallery/g2.jpg',
-      caption: 'An investment in knowledge pays the best Interests.',
-    },
-    {
-      image: '../../../assets/gallery/g3.jpg',
-      caption: 'If you can dream it, you can do it.',
+      image: '../../../assets/gallery/g1.jpg',
+      caption: 'A little progress each day leads to a big result.',
     },
     {
       image: '../../../assets/gallery/g4.jpg',
       caption: 'Knowledge is key to success in life.',
     },
+    {
+      image: '../../../assets/gallery/g2.jpg',
+      caption: 'Positive and Friendly Environment.',
+    },
+    {
+      image: '../../../assets/gallery/g3.jpg',
+      caption: 'If you can dream it, you can do it.',
+    }
+   
   ];
 }
